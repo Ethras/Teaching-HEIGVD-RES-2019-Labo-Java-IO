764e1ad9c6f68a6de084b453b978a591ee4824bc
Ethras <vladimirmbassi@gmail.com>
2019-03-21 14:14:03 +0100

    FileNumberingFiltertest pass till itShouldHandleWriteWithAnInt

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -10,32 +10,45 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int lineCount = 0;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        str = str.substring(off, off + len);
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        // if first line
+        if (lineCount == 0)
+            str = ++lineCount + "\t" + str;
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        int index = -1;
+        while ((index = str.indexOf("\n", index + 1)) != -1) {
+            lineCount++;
+            str = str.substring(0, index + 1) + lineCount + "\t" + ((str.length() > index + 2) ? str.substring(index + 2) : "");
+        }
+        System.out.print(str);
+        out.write(str);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        write(new String(cbuf), off, len);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        write(String.valueOf((char) c), 0, 1);
+    }
 
 }
